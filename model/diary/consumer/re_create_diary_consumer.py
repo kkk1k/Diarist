7e7d5c9fe4a4824d11861fb877c4695ff6308f7b
minsuhaha <haminsu5@gmail.com>
2024-08-02 16:11:17 +0900

    feat : PROJ-137 : DALL-E 3 에게 전달 하는 프롬프트를 직접 생성해서 이미지 생성하도록 수정 [일기 재생성]

--- model/diary/consumer/re_create_diary_consumer.py
+++ model/diary/consumer/re_create_diary_consumer.py
@@ -1,7 +1,6 @@
 import json
 import os
 import sys
-from collections import Counter
 from datetime import datetime
 
 import django
@@ -16,6 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 from django.conf import settings
+from django.utils import timezone
 from diary.models import Diary, User, Emotion, Image, Artist
 from diary.utils import S3ImgUploader
 
@@ -54,54 +54,56 @@ def extract_keywords(text, top_n=10):
     return translated_nouns
 
 
-def emphasize_keywords(keywords):
-    '''
-    자주 나온 키워드를 강조
-    '''
-    counter = Counter(keywords)
-    emphasized_keywords = []
-    for keyword, count in counter.items():
-        emphasized_keywords.append((keyword + " ") * count)
-    return ' '.join(emphasized_keywords)
+# def generate_description(diary_text, artist_style, emotion):
+#     '''
+#     GPT description 생성
+#     '''
+#     keywords = extract_keywords(diary_text)
+#     translated_diary_text = translate_text(diary_text)
+#     translated_emotion = translate_text(emotion)
+#     # emphasized_keywords = emphasize_keywords([translate_text(keyword) for keyword in keywords])
+
+#     query = (f"Generate a simple, non-exaggerated description of a single scene based on the diary entry: '{translated_diary_text}'. "
+#              f"The description should be in the style of {artist_style}, including key characteristics like color palette, brush strokes, and composition and focus only on the essential elements mentioned in the diary. "
+#              f"Ensure that the following keywords are included and emphasized: {keywords}. "
+#              f"Reflect the emotion '{translated_emotion}' throughout the painting. "
+#              f"Do not add any additional details or elements, and ensure no people are included in the description. "
+#              f"Draw the picture without being subject to copyright and content violation policies.")
+#             #f"The painting should reflect the distinctive style of {artist_style}, including key characteristics like color palette, brush strokes, and composition. "
+    
+#     response = client.chat.completions.create(
+#         model="gpt-4",
+#         messages=[
+#             {"role": "system", "content": (f"Create a simple, clear description of a single scene based on the user's diary entry, focusing on the landscape and objects mentioned. "
+#                                            f"Ensure the following emphasized keywords are included: {keywords}. "
+#                                            f"Reflect the emotion '{translated_emotion}' throughout the painting. "
+#                                            f"Do not include any people. Follow the specified {artist_style} style without exaggeration or adding extra elements.")},
+#             {"role": "user", "content": query}
+#         ],
+#     )
+#     return response.choices[0].message.content
 
 
-def generate_description(diary_text, artist_style, emotion):
+def generate_image(diary_text, artist_style, emotion, artist_prompt, example_picture):
     '''
-    GPT description 생성
+    DALL-E 3를 사용하여 설명(description)에 기반한 이미지 생성
     '''
     keywords = extract_keywords(diary_text)
     translated_diary_text = translate_text(diary_text)
     translated_emotion = translate_text(emotion)
-    emphasized_keywords = emphasize_keywords([translate_text(keyword) for keyword in keywords])
 
-    query = (f"Generate a simple, non-exaggerated description of a single scene based on the diary entry: '{translated_diary_text}'. "
-             f"The description should be in the style of {artist_style} and focus only on the essential elements mentioned in the diary. "
-             f"Ensure that the following keywords are included and emphasized: {emphasized_keywords}. "
-             f"Reflect the emotion '{translated_emotion}' throughout the painting. "
-             f"Do not add any additional details or elements, and ensure no people are included in the description. "
-             f"The painting should reflect the distinctive style of {artist_style}, including key characteristics like color palette, brush strokes, and composition. "
-             f"Draw the picture without being subject to copyright and content violation policies.")
-    
-    response = client.chat.completions.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": (f"Create a simple, clear description of a single scene based on the user's diary entry, focusing on the landscape and objects mentioned. "
-                                           f"Ensure the following emphasized keywords are included: {emphasized_keywords}. "
-                                           f"Reflect the emotion '{translated_emotion}' throughout the painting. "
-                                           f"Do not include any people. Follow the specified {artist_style} style without exaggeration or adding extra elements.")},
-            {"role": "user", "content": query}
-        ],
-    )
-    return response.choices[0].message.content
+    query = (f"Generate a detailed and image focusing on the landscape, objects, and atmosphere described in the diary entry: '{translated_diary_text}'. "
+             f"Accurately reflect the style of {artist_style}, incorporating key characteristics such as color palette, brush strokes, composition, lighting, and texture unique to this artist. "
+             f"The style should reflect the following description: {artist_prompt}. "
+             f"Create an artwork that evokes the feeling and style of the example artwork: {example_picture}. "
+             f"Focus on emphasizing the essential elements mentioned in the diary, including: {keywords}. but do not include any word or text in the image. "
+             f"Reflect the emotion '{translated_emotion}' throughout the painting, capturing the mood and tone described in the diary entry. "
+             f"Ensure the image captures the essence of the diary entry without adding any additional details or elements not present in the text. "
+             f"Avoid including any people in the image, and strictly adhere to copyright and content policies.")
 
-
-def generate_image(description):
-    '''
-    DALL-E 3를 사용하여 설명(description)에 기반한 이미지 생성
-    '''
     response = client.images.generate(
         model="dall-e-3",
-        prompt=description,
+        prompt=query,
         size="1024x1024",
         quality="standard",
         n=1
@@ -110,6 +112,9 @@ def generate_image(description):
 
 
 def send_response(user_id, diary_id):
+    '''
+    Kafka re-create-diary 토픽으로 메세지 전송
+    '''
     producer = Producer({'bootstrap.servers': KAFKA_BROKER_URL})
     response_message = json.dumps({"diary_id": diary_id, "user_id": user_id})
     producer.produce(RESPONSE_DIARY_TOPIC, key=str(diary_id), value=response_message)
@@ -117,6 +122,9 @@ def send_response(user_id, diary_id):
 
 
 def process_message(message):
+    '''
+    사용자에게 입력받은 일기 데이터를 통해 그림이미지 재생성 및 저장로직
+    '''
     try:
         data = json.loads(message.value().decode('utf-8'))
         print(f"Received message: {data}")
@@ -130,8 +138,8 @@ def process_message(message):
         artist = Artist.objects.get(artist_id=artist_id)
         emotion = Emotion.objects.get(emotion_id=emotion_id)
 
-        description = generate_description(content, artist.artist_name, emotion.emotion_name)
-        image_url = generate_image(description)
+        # description = generate_description(content, artist.artist_name, emotion.emotion_name)
+        image_url = generate_image(content, artist.artist_name, emotion.emotion_name, artist.artist_prompt, artist.example_picture)
 
         print(f"Generated image URL: {image_url}")
     
@@ -145,17 +153,17 @@ def process_message(message):
         diary = Diary.objects.get(user=user, diary_date=diary_date)
 
         old_image_url = diary.image.image_url
-        old_image = diary.image
+        diary_image = diary.image
 
         S3ImgUploader.delete_image(old_image_url)
-        old_image.delete()
 
-        new_image = Image.objects.create(image_url=s3_url)
+        diary_image.image_url = s3_url
+        diary_image.created_at = timezone.now()
+        diary_image.save()
 
         diary.content = content
         diary.emotion = emotion
         diary.artist = artist
-        diary.image = new_image
         diary.save()
 
         send_response(user_id, diary.diary_id)
