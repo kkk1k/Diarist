0b8418a10383fc6238256d723c64ffecc40a5de1
Jmboy713 <jimin713@naver.com>
2024-08-06 11:11:17 +0900

    refactor : PROJ-172 : util class 인스턴스 불가 선언

--- server/Spring/src/main/java/com/hanium/diarist/common/security/jwt/JwtProperties.java
+++ server/Spring/src/main/java/com/hanium/diarist/common/security/jwt/JwtProperties.java
@@ -1,6 +1,11 @@
 package com.hanium.diarist.common.security.jwt;
 
 public class JwtProperties {
+
+    private JwtProperties() {
+        throw new IllegalStateException("util class는 인스턴스화 할 수 없습니다.");
+    }
+
     public static final String JWT_TOKEN_HEADER = "Authorization";
 
     public static final String USER_ID="userId";
