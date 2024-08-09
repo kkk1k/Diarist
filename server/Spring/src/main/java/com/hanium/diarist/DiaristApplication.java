56069537c98ead1952726bcc160e3371775fa764
jmboy713 <jimin713@naver.com>
2024-08-09 14:44:33 +0900

    feat : PROJ-133 : yml 파일의 필요한 부분을 .env을 통해 가져오지 않고 gitaction의 secret에 yml 파일을 넣어서 사용

--- server/Spring/src/main/java/com/hanium/diarist/DiaristApplication.java
+++ server/Spring/src/main/java/com/hanium/diarist/DiaristApplication.java
@@ -1,6 +1,5 @@
 package com.hanium.diarist;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,25 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DiaristApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("KAFKA_BOOTSTRAP_SERVERS", dotenv.get("KAFKA_BOOTSTRAP_SERVERS"));
-        System.setProperty("KAKAO_REST_API_KEY", dotenv.get("KAKAO_REST_API_KEY"));
-        System.setProperty("KAKAO_CLIENT_SECRET", dotenv.get("KAKAO_CLIENT_SECRET"));
-        System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
-        System.setProperty("KAKAO_ADMIN_KEY", dotenv.get("KAKAO_ADMIN_KEY"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-        System.setProperty("GOOGLE_REDIRECT_URI", dotenv.get("GOOGLE_REDIRECT_URI"));
-        System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-        System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-        System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET"));
-        System.setProperty("AWS_ENDPOINT", dotenv.get("AWS_ENDPOINT"));
-
 
         SpringApplication.run(DiaristApplication.class, args);
     }
--- server/Spring/src/test/java/com/hanium/diarist/DiaristApplicationTests.java
+++ server/Spring/src/test/java/com/hanium/diarist/DiaristApplicationTests.java
@@ -1,16 +1,12 @@
 package com.hanium.diarist;
 
-import com.hanium.diarist.common.config.DotenvConfig;
-import com.hanium.diarist.common.config.TestEnvironmentConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 
 @SpringBootTest
 @ActiveProfiles("test")
-@ContextConfiguration(classes = {TestEnvironmentConfig.class, DotenvConfig.class})
 class DiaristApplicationTests {
 
     @Test
